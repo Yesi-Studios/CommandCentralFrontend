cb1942aff3c41f744f23f0783906e32c4eca29c0
CTI2(IDW) Angus McLean <anguslmm@gmail.com>
2016-06-30 10:37:05 -0400

    Set up muster.html

--- CommandCentralFrontend/modules/muster/controllers.js
+++ CommandCentralFrontend/modules/muster/controllers.js
@@ -8,6 +8,19 @@ angular.module('Muster')
 		
         // This scope will just about always contain PII
         $rootScope.containsPII = true;
+        
+        // The default sorting key
+        $scope.orderKey = "LastName";
+
+        // Give our scope a way to sort
+        $scope.setOrder = function (theKey) {
+            if ($scope.orderKey == theKey) {
+                $scope.orderKey = "-" + theKey;
+            } else {
+                $scope.orderKey = theKey;
+            }
+        }
+
 		}
 		
     ]);
