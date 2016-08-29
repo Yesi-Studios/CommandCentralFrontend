178ffde46af488203b6380d0f9c0c0d305d1ad69
CTI2(IDW) Angus McLean <anguslmm@gmail.com>
2016-08-29 13:37:49 -0400

    Fix continue links.

--- CommandCentralFrontend/modules/authentication/services.js
+++ CommandCentralFrontend/modules/authentication/services.js
@@ -41,7 +41,7 @@ angular.module('Authentication')
 		};
 
 		service.BeginRegistration = function (ssn, success, error) {
-		    return ConnectionService.RequestFromBackend('BeginRegistration', { 'ssn': ssn, 'continuelink' : 'http://127.0.0.1:63342/CommandCentralFrontend/CommandCentralFrontend/index.html?_ijt=e1df7pk3ivipdtv8jreojnj6nm#/finishregistration/'}, success, error);
+		    return ConnectionService.RequestFromBackend('BeginRegistration', { 'ssn': ssn, 'continuelink' : 'https://commandcentral/#/finishregistration/'}, success, error);
 		};
 
 		service.CreateUser = function (person, success, error) {
@@ -49,7 +49,7 @@ angular.module('Authentication')
 		};
 		
 		service.ForgotPassword = function (email, ssn, success, error) {
-		    return ConnectionService.RequestFromBackend('BeginPasswordReset', { 'email': email, 'ssn': ssn }, success, error);
+		    return ConnectionService.RequestFromBackend('BeginPasswordReset', { 'email': email, 'ssn': ssn, 'continuelink' : 'https://commandcentral/#/finishreset/' }, success, error);
 		};
 		
 		service.FinishReset = function (password, id, success, error) {
