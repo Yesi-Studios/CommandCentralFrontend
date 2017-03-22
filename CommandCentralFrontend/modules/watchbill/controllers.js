de26f2637e59e83f21baadecf16fe8c29e447ccf
CTI2(IDW) Angus McLean <anguslmm@gmail.com>
2017-03-22 01:18:47 -0400

    Start watchbill directives.

--- CommandCentralFrontend/modules/watchbill/controllers.js
+++ CommandCentralFrontend/modules/watchbill/controllers.js
@@ -1,3 +1,33 @@
-/**
- * Created by Angus on 3/20/2017.
- */
+'use strict';
+
+angular.module('Watchbill')
+
+    .controller('WatchbillController',
+        ['$scope', '$rootScope', '$location', '$routeParams', 'AuthenticationService', 'ProfileService', 'AuthorizationService', 'ConnectionService', 'WatchbillService',
+            function ($scope, $rootScope, $location, $routeParams, AuthenticationService, ProfileService, AuthorizationService, ConnectionService, WatchbillService) {
+
+            }]
+    )
+    .directive('ngWatchBillNub', function () {
+            return {
+                restrict: 'E',
+                require: '^ngModel',
+                scope: {
+                    ngModel: '=',
+                    watchbillId: '@'
+                },
+                templateUrl: "modules/watchbill/directives/watchbillnub.html",
+                controller: ['$scope', '$location', 'WatchbillService', 'ConnectionService', function ($scope, $location, WatchbillService, ConnectionService) {
+                    WatchbillService.LoadWatchbill($scope.watchbillId,
+                        function (response) {
+                            $scope.watchbill = response;
+                        },
+                        // If we fail, this is our call back. We use a convenience function in the ConnectionService.
+                        function (response) {
+                            ConnectionService.HandleServiceError(response, $scope, $location);
+                        }
+                    );
+                }]
+            }
+        }
+    );
--- CommandCentralFrontend/modules/watchbill/services.js
+++ CommandCentralFrontend/modules/watchbill/services.js
@@ -1,3 +1,18 @@
 /**
  * Created by Angus on 3/20/2017.
  */
+'use strict';
+
+angular.module('Watchbill')
+
+    .factory('WatchbillService',
+        ['$http', '$localStorage', '$rootScope', '$timeout', 'AuthenticationService', 'ConnectionService',
+            function ($http, $localStorage, $rootScope, $timeout, AuthenticationService, ConnectionService) {
+                var service = {};
+
+                service.LoadWatchbill = function (id, success, error) {
+                    return ConnectionService.RequestFromBackend('LoadWatchbill', {'watchbillId': id, 'authenticationtoken': AuthenticationService.GetAuthToken()}, success, error);
+                };
+
+                return service;
+            }]);
