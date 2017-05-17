72886a5516c854ae4a10ab1456a65b335ea1b162
CTI2(IDW) Angus McLean <anguslmm@gmail.com>
2017-05-17 17:47:14 -0400

    Random updates that I totally shouldn't even be doing on this branch... about to merge.

--- modules/connection/services.js
+++ modules/connection/services.js
@@ -9,12 +9,7 @@ angular.module('Connection')
                 var redirectURL = '';
                 // Information for connecting to the database, including the URL and API key.
                 var apikey = config.apiKey;
-                //var backendURL = "http://73.20.152.170";  // Atwood's IP for working at home.
-                //var backendURL = "http://147.51.62.19";     // Dev service IP.
-                //var backendURL = "https://147.51.62.48:1337";     // Live service IP.
-                //var backendURL = "http://192.168.1.234";
                 var backendURL = config.backendURL;
-                //var backendURL = "http://192.168.1.196";
 
                 // Here we check to see if we have a port stored in localStorage, and if not, we use 1113.
                 // If it's enabled, there's a widget at the bottom of index.html, controlled by this module,
--- modules/faq/controllers.js
+++ modules/faq/controllers.js
@@ -2,81 +2,85 @@
 
 angular.module('FAQ')
 
-.controller('FAQController',
-    ['$scope', '$rootScope', 'config', 'FAQService', 'AuthorizationService',
-        function ($scope, $rootScope, config, FAQService, AuthorizationService) {
+    .controller('FAQController',
+        ['$scope', '$rootScope', '$location', 'config', 'FAQService', 'AuthorizationService', 'ConnectionService',
+            function ($scope, $rootScope, $location, config, FAQService, AuthorizationService, ConnectionService) {
+                var errorHandler = function (response) {
+                    ConnectionService.HandleServiceError(response, $scope, $location);
+                };
 
-            $scope.userCanEditFAQs = function () { return AuthorizationService.CanEditFAQs(); };
+                $scope.userCanEditFAQs = function () {
+                    return AuthorizationService.CanEditFAQs();
+                };
 
-            $scope.deleteFAQ = function (faq) {
-                FAQService.DeleteFAQ(faq, function(response) {
-                        loadFAQs();
-                    },
-                    // If we fail, this is our call back. We use a convenience function in the ConnectionService.
-                    function (response) {
-                        ConnectionService.HandleServiceError(response, $scope, $location);
-                    });
+                $scope.deleteFAQ = function (faq) {
+                    FAQService.DeleteFAQ({Id: "e614698a-93dd-4047-a3bc-d09043f705ce"}, function (response) {
+                            loadFAQs();
+                        }, errorHandler);
+                };
+
+                $scope.dataLoading = true;
+                var loadFAQs = function () {
+                    FAQService.LoadFAQs(
+                        function (response) {
+                            $scope.dataLoading = false;
+                            $scope.questions = response.ReturnValue;
+                            $scope.loadedTime = new Date;
+                        },
+                        // If we fail, this is our call back. We use a convenience function in the ConnectionService.
+                        function (response) {
+                            ConnectionService.HandleServiceError(response, $scope, $location);
+                        }
+                    );
+                };
+
+                loadFAQs();
+
+            }])
+    .controller('CreateFAQController', ['$scope', '$rootScope', '$location', '$routeParams', 'AuthenticationService', 'FAQService', 'ConnectionService', 'AuthorizationService',
+        function ($scope, $rootScope, $location, $routeParams, AuthenticationService, FAQService, ConnectionService, AuthorizationService) {
+
+            var errorHandler = function (response) {
+                ConnectionService.HandleServiceError(response, $scope, $location);
             };
 
-            $scope.dataLoading = true;
-            var loadFAQs = function () {
-                FAQService.LoadFAQs(
+            $scope.userCanEditFAQs = function () {
+                return AuthorizationService.CanEditFAQs();
+            };
+
+            if ($routeParams.id) {
+                $scope.dataLoading = true;
+                FAQService.LoadFAQ($routeParams.id,
                     function (response) {
                         $scope.dataLoading = false;
-                        $scope.questions = response.ReturnValue;
-                        $scope.loadedTime = new Date;
-                    },
-                    // If we fail, this is our call back. We use a convenience function in the ConnectionService.
+                        $scope.faqName = response.ReturnValue.Name;
+                        $scope.faqQuestion = response.ReturnValue.Question;
+                        $scope.faqBody = response.ReturnValue.Paragraphs.join('\n');
+                    }, errorHandler);
+
+            }
+
+            $scope.saveFAQ = function (name, question, body) {
+                $scope.dataLoading = true;
+
+                // Create a new FAQ with what we got
+                var newFAQ = {
+                    'Id': $routeParams.id,
+                    'Name': name,
+                    'Question': question,
+                    'Paragraphs': body.match(/[^\r\n]+/g)
+                };
+
+                // If we aren't editing an existing FAQ, remove the Id keyword for safety
+                if (!$routeParams.id) { delete newFAQ['Id']; }
+
+                // Do the Update
+                FAQService.CreateOrUpdateFAQ(newFAQ,
                     function (response) {
-                        ConnectionService.HandleServiceError(response, $scope, $location);
-                    }
-                );
+                        $scope.dataLoading = false;
+                        $location.path('/faq');
+
+                    }, errorHandler);
             };
 
-            loadFAQs();
-
-        }])
-.controller('CreateFAQController', ['$scope', '$rootScope', '$location', '$routeParams', 'AuthenticationService', 'FAQService', 'ConnectionService', 'AuthorizationService',
-    function ($scope, $rootScope, $location, $routeParams, AuthenticationService, FAQService, ConnectionService, AuthorizationService) {
-
-        $scope.userCanEditFAQs = function () { return AuthorizationService.CanEditFAQs(); };
-
-       if($routeParams.id) {
-           $scope.dataLoading = true;
-           FAQService.LoadFAQ($routeParams.id,
-               function(response){
-                    $scope.dataLoading = false;
-                   $scope.faqName= response.ReturnValue.Name;
-                   $scope.faqQuestion = response.ReturnValue.Question;
-                   $scope.faqBody = response.ReturnValue.Paragraphs.join('\n');
-               },
-               // If we fail, this is our call back. We use a convenience function in the ConnectionService.
-               function (response) {
-                   ConnectionService.HandleServiceError(response, $scope, $location);
-               }
-           );
-
-        }
-
-        $scope.saveFAQ = function (name, question, body) {
-            $scope.dataLoading = true;
-            var newFAQ;
-            if($routeParams.id) {
-                newFAQ = {'Id': $routeParams.id, 'Name':name, 'Question': question, 'Paragraphs': body.match(/[^\r\n]+/g)};
-            } else {
-                newFAQ = {'Name':name, 'Question': question, 'Paragraphs': body.match(/[^\r\n]+/g)};
-            }
-            FAQService.CreateOrUpdateFAQ(newFAQ,
-                function (response) {
-                    $scope.dataLoading = false;
-                    $location.path('/faq');
-
-                },
-                // If we fail, this is our call back. We use a convenience function in the ConnectionService.
-                function (response) {
-                    ConnectionService.HandleServiceError(response, $scope, $location);
-                }
-            );
-        };
-
-    }]);
+        }]);
